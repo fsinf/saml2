93cd5583a9224e73e50f955586b622cef9690100
David Kaufmann <astra@fsinf.at>
2020-04-20 17:14:34 +0200

    force lowercase usernames

--- idp/backends.py
+++ idp/backends.py
@@ -3,5 +3,17 @@ from django_pam.auth.backends import PAMBackend
 
 class PAMRestAuthBackend(PAMBackend):
     def authenticate(self, *args, **kwargs):
-        kwargs.pop('service', None)
-        return super().authenticate(*args, service='saml2', **kwargs)
+        service = kwargs.pop('service', None)
+        username = kwargs.pop('username', None)
+        password = kwargs.pop('password', None)
+        extra_fields = kwargs.pop('extra_fields', None)
+        if username is None or password is None:
+            return None
+        username_lower = username.lower()
+        user = super().authenticate(*args, service='saml2', username=username_lower, password=password, extra_fields=extra_fields)
+        if user is None:
+            return None
+        if user.email is None:
+            user.email = f"{username_lower}@fsinf.at"
+            user.save()
+        return user
